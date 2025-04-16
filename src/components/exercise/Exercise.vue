316b9a887676b4f007de3b5622b02440981da92d
Nebich <emilien.paris348@gmail.com>
2025-04-17 00:10:45 +0200

    feat: composant Exercice

---
+++ src/components/exercise/Exercise.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+    defineProps<{
+        name: string
+        rpe: number
+        repetitions: number
+        sets: number
+    }>()
+</script>
+
+<template>
+    <div class="flex flex-col">
+        <p class="font-bold text-blue-800">{{ name }} - RPE {{ rpe }}</p>
+        <span class="text-gray-500">Série(s): {{ sets }}</span>
+        <span class="text-gray-500">Répétitions: {{ repetitions }}</span>
+    </div>
+</template>
