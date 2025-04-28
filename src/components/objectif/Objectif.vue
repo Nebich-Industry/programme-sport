34396d613b27b2ba9a69a8df6b18350936c0053e
Nebich <emilien.paris348@gmail.com>
2025-04-29 00:19:36 +0200

    feat: consultation des objectifs

---
+++ src/components/objectif/Objectif.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import type { ObjectifModel } from '@/model/ObjectifModel';
+
+defineProps<ObjectifModel>()
+</script>
+<template>
+    <p class="flex items-center text-red-600 px-14 w-full text-nowrap pb-2">
+        <span class="pi pi-bullseye pr-2"></span>
+        {{ objectif }}
+    </p>
+</template>
