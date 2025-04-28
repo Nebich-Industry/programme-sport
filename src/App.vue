cbfde31128d12764b0b6a299d0c4235123711a96
Nebich <emilien.paris348@gmail.com>
2025-04-28 23:24:44 +0200

    feat: message d'arrivée

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import Bonjour from './components/bonjour/Bonjour.vue';
 import Seance from './components/seance/Seance.vue'
 </script>
 
 <template>
-  <div class="flex justify-center">
+  <div class="flex flex-col justify-center items-center">
+    <Bonjour :message="'Bienvenue Utilisateur,'" :a-une-seance-prevue="true" />
     <Seance />
   </div>
 </template>
---
+++ src/components/bonjour/Bonjour.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import type { MessageArriveeModel } from '@/model/MessageArriveeModel';
+defineProps<MessageArriveeModel>();
+</script>
+
+<template>
+    <div class="flex flex-col">
+        <p>{{ message }}</p>
+        <p v-if="aUneSeancePrevue">Voici vote séance du jour :</p>
+    </div>
+</template>
